fa319bc606829afaf8722f64a56458e5ac5c1cf1
Timshel Knoll-Miller <timshel@shels.net>
2015-12-01 15:40:01 +1000

    Allow backslash-escaping of [[...]] tags

--- test/spec/rendererSpec.js
+++ test/spec/rendererSpec.js
@@ -43,6 +43,41 @@ describe ("Renderer", function() {
     expect(Renderer.render(text)).to.be.equal("<p><a class=\"internal\" href=\"/wiki/Foo\">Foo</a>, <a class=\"internal\" href=\"/wiki/Bar\">Bar</a></p>\n");
   });
 
+  it ("should not render escaped tags 1", function() {
+    var text = "a \\[[Foo]] b";
+    expect(Renderer.render(text)).to.be.equal("<p>a [[Foo]] b</p>\n");
+  });
+
+  it ("should not render escaped tags 2", function() {
+    var text = "a \\[[Foo]][[Bar]] b";
+    expect(Renderer.render(text)).to.be.equal("<p>a [[Foo]]<a class=\"internal\" href=\"/wiki/Bar\">Bar</a> b</p>\n");
+  });
+
+  it ("should not render escaped tags 3", function() {
+    var text = "a [[Foo]]\\[[Bar]] b";
+    expect(Renderer.render(text)).to.be.equal("<p>a <a class=\"internal\" href=\"/wiki/Foo\">Foo</a>[[Bar]] b</p>\n");
+  });
+
+  it ("should not render escaped tags 4", function() {
+    var text = "a \\[[Foo]]\\[[Bar]] b";
+    expect(Renderer.render(text)).to.be.equal("<p>a [[Foo]][[Bar]] b</p>\n");
+  });
+
+  it ("should not render escaped tags 5", function() {
+    var text = "a \\[[Foo / Bar]] b";
+    expect(Renderer.render(text)).to.be.equal("<p>a [[Foo / Bar]] b</p>\n");
+  });
+
+  it ("should render tags after escaped backslash 1", function() {
+    var text = "a \\\\[[Foo]] b";
+    expect(Renderer.render(text)).to.be.equal("<p>a \\<a class=\"internal\" href=\"/wiki/Foo\">Foo</a> b</p>\n");
+  });
+
+  it ("should not render escaped tags 1", function() {
+    var text = "a \\\\\\[[Foo]] b";
+    expect(Renderer.render(text)).to.be.equal("<p>a \\[[Foo]] b</p>\n");
+  });
+
   it ("should replace {{TOC}} with the table of contents", function() {
     var text = "{{TOC}}\n\n # Heading 1 \n\n This is some text";
     expect(Renderer.render(text)).to.be.equal("<ul>\n<li><p><a href=\"#heading-1\">Heading 1</a></p>\n<h1 id=\"heading-1\">Heading 1</h1>\n<p>This is some text</p>\n</li>\n</ul>\n");
